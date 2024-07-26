bf94a29e7069b081a57eca2fe1301161ec8a3340
Charul <charuljain911@gmail.com>
2024-07-26 10:04:09 +0530

    debug

--- fitness/fitness/urls.py
+++ fitness/fitness/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .import views,trainer_views,customer_views
+from .import views,trainer_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
